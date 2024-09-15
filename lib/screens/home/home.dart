2bb91dd3e9649106727089ce4e1a3fd9a152a167
ltmpofudev <140100661+ltmpofudev@users.noreply.github.com>
2024-09-15 10:46:58 -0400

    Sunday Edits
    - changed ui to something nicer; uses a page_view_dot indicator with safearea for smoother design

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,9 @@
 //homescreen of application
-
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:page_view_dot_indicator/page_view_dot_indicator.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,8 +13,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with TickerProviderStateMixin {
-  final PageController _pageControl = PageController();
-  int currentIndex = 0;
+  late int selectedPage;
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    selectedPage = 0;
+    _pageController = PageController(initialPage: selectedPage);
+
+    super.initState();
+  }
 
   final List<IconData> icons = [
     CupertinoIcons.mic,
@@ -36,9 +44,74 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    const pageCount = 5;
+
     return Scaffold(
       backgroundColor: Colors.cyan.shade500,
-      body: Container(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                  controller: _pageController,
+                  onPageChanged: (page) {
+                    setState(() {
+                      selectedPage = page;
+                    });
+                  },
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "CLARITY",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 45.0,
+                              letterSpacing: 1.0),
+                        ),
+                        const Text(
+                          "SWIPE TO START",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 35.0,
+                              letterSpacing: 1.0),
+                        ),
+                      ],
+                    ),
+                    ...List.generate(pageCount, (index) {
+                      return Center(
+                        child: Text('Page $index'),
+                      );
+                    }),
+                  ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: PageViewDotIndicator(
+                currentItem: selectedPage,
+                count: pageCount,
+                unselectedColor: Colors.black26,
+                selectedColor: Colors.blue,
+                duration: const Duration(milliseconds: 200),
+                boxShape: BoxShape.rectangle,
+                onItemClicked: (index) {
+                  _pageController.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
+      ),
+      /*Container(
         padding: const EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 30.0),
         child: ListView(
           children: [
@@ -96,37 +169,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
             )
           ],
         ),
-      ),
-      bottomNavigationBar: SalomonBottomBar(
-        currentIndex: currentIndex,
-        onTap: (i) => setState(() {
-          _pageControl.jumpToPage(i);
-          currentIndex = _pageControl.page!.round();
-        }
-        ),
-        items: [
-          /// Home
-          SalomonBottomBarItem(
-            icon: Icon(Icons.speaker),
-            title: Text("TTS"),
-            selectedColor: Colors.purple,
-          ),
-
-          /// Likes
-          SalomonBottomBarItem(
-            icon: Icon(Icons.search),
-            title: Text("TM"),
-            selectedColor: Colors.pink,
-          ),
-
-          /// Search
-          SalomonBottomBarItem(
-            icon: Icon(Icons.text_fields_rounded),
-            title: Text("ITS"),
-            selectedColor: Colors.orange.shade900,
-          ),
-        ],
-      ),
+      ),*/
     );
   }
 }
